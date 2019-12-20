e9190c31104a9e1f10e4d2c8a365f283caf476f3
carln <carln@hootch-99.com>
2019-12-20 18:35:52 -0500

    Moved battery level code to seperate file BatteryLevel.h

--- BT8_SI_OPT_Sketch.ino
+++ BT8_SI_OPT_Sketch.ino
@@ -38,124 +38,30 @@
 //#define MY_PASSIVE_NODE // This node does not listen for messages from the controller
 
 #define MY_RADIO_NRF5_ESB
+#define MY_SIGNING_SOFT
+
+/**
+* @def MY_SIGNING_REQUEST_SIGNATURES
+* @brief Enable this to inform gateway to sign all messages sent to this node.
+*
+* If used for a gateway, gateway will only request signatures from nodes that in turn
+* request signatures from gateway.
+*/
+//#define MY_SIGNING_REQUEST_SIGNATURES
+
+#define MY_SIGNING_SOFT_RANDOMSEED_PIN 29
+
+#define CR2032
+#define SENDVOLTAGE
 
 // LIBRARIES
 #include <stdio.h>
 #include "SEGGER_RTT.h"
 #include <MySensors.h> // The MySensors library. Hurray!
 #include "MySensorsNRF5setup.h"
+#include "BatteryLevel.h"
 
-/*
-//Battery Level Set Up (LiFePo4)
-#define BAT_RANGE 3.0 // BAT_HIGH - BAT_LOW in mv divided by 100 (to facilitate conversion to percent)
-#define BAT_HIGH 3350 // Fully charged battery pack in mV (lifepo4)
-#define BAT_LOW 3050  //Drained battery pack in mV (this is fine for controller, but min for BME280 and for the LiFePo4 battery)
-*/
 
-//Battery Level Set Up (CR2032)
-#define BAT_RANGE 17.0 // BAT_HIGH - BAT_LOW in mv divided by 100 (to facilitate conversion to percent)
-#define BAT_HIGH 3400  // Fully charged battery pack in mV (CR2032)
-#define BAT_LOW 1700   //Drained battery pack in mV
-
-#define SENDVOLTAGE
-
-class BatteryLevel
-{
-  // Class Member Variables
-  // These are initialized at startup
-  uint8_t BatInt;
-  float Bat;
-  float lastBat; // static to preserve from one call of the function to the next.
-#ifdef SENDVOLTAGE
-  MyMessage Msg_battery;
-#endif //SENDVOLTAGE
-
-  // These maintain the current state
-  unsigned long previousMillis, intervalBat;
-
-public:
-  // Default Constructor
-  BatteryLevel()
-  {
-    intervalBat = 1800000;
-    previousMillis = 0;
-#ifdef SENDVOLTAGE
-    Msg_battery.setSensor(244);
-    Msg_battery.setType(V_VOLTAGE);
-#endif //SENDVOLTAGE
-  }
-
-  BatteryLevel(const uint8_t sensor, unsigned long interval)
-  {
-    intervalBat = interval;
-    previousMillis = 0;
-#ifdef SENDVOLTAGE
-    Msg_battery.setSensor(sensor);
-    Msg_battery.setType(V_VOLTAGE);
-#endif //SENDVOLTAGE
-  }
-
-  void update(unsigned long current)
-  {
-    // Store the current millis
-    unsigned long currentMillis = current;
-
-    Bat = (float)hwCPUVoltage(); //take voltage measurement
-#ifdef MY_DEBUG
-    Serial.print("\nBat //  lastBat:  ");
-    Serial.print(Bat);
-    Serial.print(" //  ");
-    Serial.println(lastBat);
-    SEGGER_RTT_printf(0, "Bat // lastBat: %u // %u.\r\n", Bat, lastBat);
-
-#endif // MY_DEBUG
-
-    if (Bat > BAT_LOW)
-    {
-#ifdef MY_DEBUG
-      Serial.println("Bat > BAT_LOW");
-      SEGGER_RTT_WriteString(0, "\r\nBat > BAT_LOW\r\n");
-
-#endif // MY_DEBUG
-      if ((currentMillis - previousMillis) > intervalBat)
-      {
-        BatInt = (uint8_t)((Bat - BAT_LOW) / BAT_RANGE);
-        sendBatteryLevel(BatInt);
-#ifdef SENDVOLTAGE
-        send(Msg_battery.set(Bat / 1000.0, 3)); //send battery voltage with 3 decimal places
-#endif                                          //SENDVOLTAGE
-        previousMillis = currentMillis;
-#ifdef MY_DEBUG
-        Serial.print("\nBat //  BatInt:  ");
-        Serial.print(Bat);
-        Serial.print(" //  ");
-        Serial.println(BatInt);
-        SEGGER_RTT_printf(0, "Bat // BatInt: %u // %u.\r\n", Bat, BatInt);
-
-#endif // MY_DEBUG
-
-        lastBat = Bat;
-      }
-      else
-      {
-#ifdef MY_DEBUG
-        Serial.println("not time yet.  No bat update");
-        SEGGER_RTT_WriteString(0, "\r\nNot time yet.  No bat update\r\n");
-#endif // MY_DEBUG
-      }
-    }
-    else
-    { //Bat <= BAT_LOW
-      sendBatteryLevel(0);
-#ifdef SENDVOLTAGE
-      send(Msg_battery.set(Bat / 1000.0, 3)); //send battery voltage with 3 decimal places
-#endif                                        //ID_BATTERY
-#ifdef LIFEPO4
-      sleep(0); //Go to sleep forever to try and save the battery (bad for LifePo4 to get less than 2.8V)
-#endif          //LIFEPO4
-    }
-  }
-};
 #define SHORT_WAIT 50
 #define SKETCH_NAME "BT8_SQ"
 #define SKETCH_VERSION "v0.1"
---
+++ BatteryLevel.h
@@ -0,0 +1,114 @@
+
+
+#ifdef LIFEPO4
+//Battery Level Set Up (LiFePo4)
+#define BAT_RANGE 3.0 // BAT_HIGH - BAT_LOW in mv divided by 100 (to facilitate conversion to percent)
+#define BAT_HIGH 3350 // Fully charged battery pack in mV (lifepo4)
+#define BAT_LOW 3050  //Drained battery pack in mV (this is fine for controller, but min for BME280 and for the LiFePo4 battery)
+#endif
+
+#ifdef CR2032
+//Battery Level Set Up (CR2032)
+#define BAT_RANGE 17.0 // BAT_HIGH - BAT_LOW in mv divided by 100 (to facilitate conversion to percent)
+#define BAT_HIGH 3400  // Fully charged battery pack in mV (CR2032)
+#define BAT_LOW 1700   //Drained battery pack in mV
+#endif
+
+
+class BatteryLevel
+{
+  // Class Member Variables
+  // These are initialized at startup
+  uint8_t BatInt;
+  float Bat;
+  float lastBat; // static to preserve from one call of the function to the next.
+#ifdef SENDVOLTAGE
+  MyMessage Msg_battery;
+#endif //SENDVOLTAGE
+
+  // These maintain the current state
+  unsigned long previousMillis, intervalBat;
+
+public:
+  // Default Constructor
+  BatteryLevel()
+  {
+    intervalBat = 1800000;
+    previousMillis = 0;
+#ifdef SENDVOLTAGE
+    Msg_battery.setSensor(244);
+    Msg_battery.setType(V_VOLTAGE);
+#endif //SENDVOLTAGE
+  }
+
+  BatteryLevel(const uint8_t sensor, unsigned long interval)
+  {
+    intervalBat = interval;
+    previousMillis = 0;
+#ifdef SENDVOLTAGE
+    Msg_battery.setSensor(sensor);
+    Msg_battery.setType(V_VOLTAGE);
+#endif //SENDVOLTAGE
+  }
+
+  void update(unsigned long current)
+  {
+    // Store the current millis
+    unsigned long currentMillis = current;
+
+    Bat = (float)hwCPUVoltage(); //take voltage measurement
+#ifdef MY_DEBUG
+    Serial.print("\nBat //  lastBat:  ");
+    Serial.print(Bat);
+    Serial.print(" //  ");
+    Serial.println(lastBat);
+    SEGGER_RTT_printf(0, "Bat // lastBat: %u // %u.\r\n", Bat, lastBat);
+
+#endif // MY_DEBUG
+
+    if (Bat > BAT_LOW)
+    {
+#ifdef MY_DEBUG
+      Serial.println("Bat > BAT_LOW");
+      SEGGER_RTT_WriteString(0, "\r\nBat > BAT_LOW\r\n");
+
+#endif // MY_DEBUG
+      if ((currentMillis - previousMillis) > intervalBat)
+      {
+        BatInt = (uint8_t)((Bat - BAT_LOW) / BAT_RANGE);
+        sendBatteryLevel(BatInt);
+#ifdef SENDVOLTAGE
+        send(Msg_battery.set(Bat / 1000.0, 3)); //send battery voltage with 3 decimal places
+#endif                                          //SENDVOLTAGE
+        previousMillis = currentMillis;
+#ifdef MY_DEBUG
+        Serial.print("\nBat //  BatInt:  ");
+        Serial.print(Bat);
+        Serial.print(" //  ");
+        Serial.println(BatInt);
+        SEGGER_RTT_printf(0, "Bat // BatInt: %u // %u.\r\n", Bat, BatInt);
+
+#endif // MY_DEBUG
+
+        lastBat = Bat;
+      }
+      else
+      {
+#ifdef MY_DEBUG
+        Serial.println("not time yet.  No bat update");
+        SEGGER_RTT_WriteString(0, "\r\nNot time yet.  No bat update\r\n");
+#endif // MY_DEBUG
+      }
+    }
+    else
+    { //Bat <= BAT_LOW
+      sendBatteryLevel(0);
+#ifdef SENDVOLTAGE
+      send(Msg_battery.set(Bat / 1000.0, 3)); //send battery voltage with 3 decimal places
+#endif                                        //ID_BATTERY
+#ifdef LIFEPO4
+      sleep(0); //Go to sleep forever to try and save the battery (bad for LifePo4 to get less than 2.8V)
+#endif          //LIFEPO4
+    }
+  }
+};
